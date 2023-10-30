032eaded537866733cdbaee389d5d0eeefec7230
Nhan <nhtrnguy@ucsc.edu>
2023-10-29 21:30:15 -0700

    finishing signup, login, logout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore, collection, getDocs, setDoc } from 'firebase/firestore'
 import {
-  getAuth
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithEmailAndPassword
 } from 'firebase/auth'
 
 const firebaseConfig = {
@@ -28,7 +31,47 @@ getDocs(dbref).then((snapshot) => {
     console.log(books);
 })
 
-const tester = document.getElementsById('loginbt')
-tester.addEventListener('click', () => {
-  
+const tester = document.querySelector('.signup')
+tester.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+
+  const email = tester.email.value;
+  const password = tester.password.value;
+
+  if(!(email.toString().includes("ucsc.edu"))){
+    alert("Email doesn't contain ucsc.edu")
+  }else{
+    createUserWithEmailAndPassword(auth, email, password).then((cred) => {
+      console.log('user created: ', cred.user);
+      tester.reset();
+    }).catch((err) => {
+      alert(err.message);
+    })
+  }
+})
+
+const testLogin = document.querySelector('.login');
+testLogin.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const email = testLogin.email.value
+  const password = testLogin.password.value
+
+  signInWithEmailAndPassword(auth, email, password).then((cred) => {
+    console.log('user logged in: ', cred.user);
+    testLogin.reset();
+  }).catch((err) => {
+    alert(err.message)
+  })
+
+})
+
+const testLogout = document.querySelector('.logout');
+testLogout.addEventListener('click', (e) => {
+  signOut(auth).then(() => {
+    console.log('the user is signed out');
+  }).catch((err) => {
+    alert(err.message);
+  })
 })
